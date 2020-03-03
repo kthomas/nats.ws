971e81cae0e1ba5f59796a9a82ab954d1e993e2a
Kyle Thomas <kyle@provide.services>
2020-03-03 03:56:48 -0500

    Revert to cjs

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         path: path.resolve(__dirname),
         filename: 'dist/nats.js',
         globalObject: 'this',
-        libraryTarget: 'umd',
+        libraryTarget: 'commonjs',
         library: 'nats',
     },
     resolve: {
