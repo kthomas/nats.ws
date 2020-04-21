79bcf4fde77958c17569b0e274789c15b529aa02
Pedro Gomes <pedrogomes94@gmail.com>
2020-04-21 14:03:37 +0200

    Update transport.ts

--- src/transport.ts
+++ src/transport.ts
@@ -140,7 +140,7 @@ export class WSTransport {
                     resolve(transport);
                     setTimeout(function () {
                         transport.handlers.messageHandler(me);
-                    }, 0);
+                    }, 100);
                 }
             };
         });
